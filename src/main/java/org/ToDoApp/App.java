546cebbb74d7b58266825250b34c34ccebe2b0eb
Asakar <asakarh@gmail.com>
2021-03-05 14:49:32 +0000

    templates and properties file added

--- src/main/java/org/ToDoApp/App.java
+++ src/main/java/org/ToDoApp/App.java
@@ -6,8 +6,7 @@ package org.ToDoApp;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println();
     }
 }
