3a35156573dbc267b368cada9583335cf17ca88b
Asakar <asakarh@gmail.com>
2021-03-09 22:12:26 +0000

    ToDoItem.java basic methods added.

--- src/main/java/org/ToDoApp/model/ToDoItem.java
+++ src/main/java/org/ToDoApp/model/ToDoItem.java
@@ -7,4 +7,12 @@ public class ToDoItem {
     public ToDoItem(String task) {
         this.task = task;
     }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
 }
